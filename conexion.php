8edeac7ef871a790c6303a0179a12a635df2d4b0
Jose_Martinez <martinez022jose@gmail.com>
2020-04-22 18:04:31 -0300

    Se agrega pagina principal, estilos y conexion base de datos

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost";
+$usuario="root";
+$contraseña="";
+$bd="galeria_dinamica";
+
+$conexion = mysqli_connect($host,$usuario,$contraseña,$bd);
+
+ ?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php  
+include("conexion.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Galeria De Fotos</title>
+	<link rel="stylesheet" type="text/css" href="estiloGaleriaDinamica.css">
+</head>
+<body>
+	<header>
+		<div class="encabezado">
+			<h1>Galeria De Imagenes</h1>
+		</div>
+	</header>
+	<div class="separador"></div>
+	<section>
+		<div class="listaDeImagenes">
+			<?php
+			 $listaDeImagenes = "SELECT *FROM imagen";
+             $resultado = mysqli_query($conexion,$listaDeImagenes);
+            //$fila = mysqli_fetch_array($resultado);
+
+
+			while($fila= mysqli_fetch_assoc($resultado)) {?>
+			<div class="item">
+				<div class="cajaImagen">
+					<img src="<?php echo $fila['url'] ?>">
+				</div>
+				<div class="descripcionImagen"><?php echo $fila['descripcion'];?></div>
+		    </div><?php }?>
+		    
+		</div>
+	</section>
+
+</body>
+</html>
